e94f2685520fc1cd6dde266c500df5403b4365f5
Sneakyp33t <ccisody@gmail.com>
2022-11-16 19:14:14 -0500

    Getting Started 1

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../products';
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent implements OnInit {
+
+  constructor() { }
+  @Input() product!: Product; // The @Input() decorator indicates that the property value passes in from the component's parent, ProductListComponent.
+  ngOnInit(): void {
+  }
+
+}
